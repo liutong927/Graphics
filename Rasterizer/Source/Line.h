671f55f1eaf6972695393c3b377e45225a3e1f01
liutong927 <ltbuaa@gmail.com>
2019-05-17 18:50:25 +0800

    Draw and Fill Triangle by Line-Sweeping algorithm.

--- Rasterizer/Source/Line.h
+++ Rasterizer/Source/Line.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "../Utils/tgaimage.h"
+#include <algorithm>
 
 class Line
 {
 public:
 	// Bresenham algorithm to draw line. Draw line is a necessity for draw wireframe of model.
 	// not x0,y0 and x1,y1 are all integer pixel value, the goal is compute all pixel value(int) of this line.
-	static void DrawLine(int InX0, int InY0, int InX1, int InY1, TGAImage &InImage, TGAColor InColor)
+	static std::vector<Vec2i> DrawLine(int InX0, int InY0, int InX1, int InY1, TGAImage &InImage, TGAColor InColor)
 	{
 		// first attempt, which depends on delta
 		//for (float delta = 0.f; delta < 1.f; delta += .1f)
@@ -21,8 +22,8 @@ public:
 		// but it has fatal error for assume that x1 is bigger than x0.
 		// so for line(10, 10, 20, 20) it can be drawn, but same line but reverse the direction (20, 20, 10, 10) it does
 		// not execute!
-		// another issue is if line's height is larger than width, then percent applied to height(y axis) will causes bigger
-		// gap than x-axis.
+		// another issue is if line's height is larger than width(, then percent applied to height(y axis) will causes bigger
+		// gap than x-axis. since x increase by one pixel, it leaves gaps for y direction.
 		//for (int X = InX0; X <= InX1; X++)
 		//{
 		//	float Percent = float(X - InX0) / float(InX1 - InX0);
@@ -65,6 +66,8 @@ public:
 		// because Y is actually computed as a float, trancate it to int is not accurate.
 		// e.g., if Y is 20.9f, it is more close to 21 pixel, but still kept as 20.
 		bool bSteep = false;
+		// bookkeeping pixels of this line.
+		std::vector<Vec2i> LineVec;
 		// if line is steep, swap (x,y) to (y,x)
 		if (abs(InY1 - InY0) > abs(InX1 - InX0))
 		{
@@ -84,7 +87,7 @@ public:
 		// DError represents increment of Y-direction for one X-direction pixel.
 		// if x increase one pixel, y's increment is less than 0.5 pixel, then y's value should not change.
 		// if it is more than 0.5 pixel, then it is more near to y+1 pixel.
-		float DError = float(DY) / float(DX);
+		float DError = std::abs(float(DY) / float(DX));
 		float Error = .0f;
 		int Y = InY0;
 		// increase one pixel in x-direction
@@ -92,10 +95,12 @@ public:
 		{
 			if (!bSteep)
 			{
+				LineVec.push_back(Vec2i(X, Y));
 				InImage.set(X, Y, InColor);
 			}
 			else
 			{
+				LineVec.push_back(Vec2i(Y, X));
 				InImage.set(Y, X, InColor);
 			}
 
@@ -107,6 +112,194 @@ public:
 				Y += InY1 > InY0 ? 1 : -1;
 				Error -= 1.f;
 			}
+		}
+
+		return LineVec;
+	}
+
+	static std::vector<Vec2i> DrawLine(Vec2i InVert0, Vec2i InVert1, TGAImage &InImage, TGAColor InColor)
+	{
+		return DrawLine(InVert0.x, InVert0.y, InVert1.x, InVert1.y, InImage, InColor);
+	}
+
+	// draw contour of triangle
+	static void DrawTriangle2D(Vec2i InVert0, Vec2i InVert1, Vec2i InVert2, TGAImage &InImage, TGAColor InColor)
+	{
+		DrawLine(InVert0.x, InVert0.y, InVert1.x, InVert1.y, InImage, InColor);
+		DrawLine(InVert1.x, InVert1.y, InVert2.x, InVert2.y, InImage, InColor);
+		DrawLine(InVert2.x, InVert2.y, InVert0.x, InVert0.y, InImage, InColor);
+	}
+
+	// fill triangle by Line-Sweep algorithm.
+	// 1. Sort vertices of the triangle by their y - coordinates;
+	// 2. Rasterize simultaneously the left and the right sides of the triangle;
+	// 3. Draw a horizontal line segment between the left and the right boundary points.
+	static void DrawAndFillTriangle2D(Vec2i InVert0, Vec2i InVert1, Vec2i InVert2, TGAImage &InImage, TGAColor InColor)
+	{
+		// first attempt:
+		/*
+		// sort the vertices of triangle.
+		// define 0 is top vertex(with max y value), 1 is left-bottom vertex, 2 is right-bottom vertex.
+		if (InVert0.y < InVert1.y)
+		{
+			std::swap(InVert0, InVert1);
+		}
+		if (InVert0.y < InVert2.y)
+		{
+			std::swap(InVert0, InVert2);
+		}
+		if (InVert1.x > InVert2.x)
+		{
+			std::swap(InVert1, InVert2);
+		}
+
+		// draw triangle line segments. sort with y descent.
+		std::vector<Vec2i> TopLeftLine = DrawLine(InVert0, InVert1, InImage, InColor);
+		std::vector<Vec2i> TopRightLine = DrawLine(InVert0, InVert2, InImage, InColor);
+		std::vector<Vec2i> LeftRightLine = DrawLine(InVert1, InVert2, InImage, InColor);
+		std::sort(TopLeftLine.begin(), TopLeftLine.end(), [](Vec2i a, Vec2i b) {return a.y > b.y; });
+		std::sort(TopRightLine.begin(), TopRightLine.end(), [](Vec2i a, Vec2i b) {return a.y > b.y; });
+		std::sort(LeftRightLine.begin(), LeftRightLine.end(), [](Vec2i a, Vec2i b) {return a.y > b.y; });
+
+		// draw horizontal line inside.
+		if (InVert1.y >= InVert2.y)
+		{
+			for (int Y = TopLeftLine[0].y; Y > InVert1.y; Y--)
+			{
+				Vec2i Start, End;
+				for (auto Vertex : TopLeftLine)
+				{
+					if (Vertex.y == Y)
+					{
+						Start = Vertex;
+						break;
+					}
+				}
+				for (auto Vertex : TopRightLine)
+				{
+					if (Vertex.y == Y)
+					{
+						End = Vertex;
+						break;
+					}
+				}
+				DrawLine(Start, End, InImage, InColor);
+			}
+			for (int Y = LeftRightLine[0].y; Y > InVert2.y; Y--)
+			{
+				Vec2i Start, End;
+				for (auto Vertex : LeftRightLine)
+				{
+					if (Vertex.y == Y)
+					{
+						Start = Vertex;
+						break;
+					}
+				}
+				for (auto Vertex : TopRightLine)
+				{
+					if (Vertex.y == Y)
+					{
+						End = Vertex;
+						break;
+					}
+				}
+				DrawLine(Start, End, InImage, InColor);
+			}
+		}
+		else
+		{
+			for (int Y = TopLeftLine[0].y; Y > InVert2.y; Y--)
+			{
+				Vec2i Start, End;
+				for (auto Vertex : TopLeftLine)
+				{
+					if (Vertex.y == Y)
+					{
+						Start = Vertex;
+						break;
+					}
+				}
+				for (auto Vertex : TopRightLine)
+				{
+					if (Vertex.y == Y)
+					{
+						End = Vertex;
+						break;
+					}
+				}
+				DrawLine(Start, End, InImage, InColor);
+			}
+			for (int Y = LeftRightLine[0].y; Y > InVert1.y; Y--)
+			{
+				Vec2i Start, End;
+				for (auto Vertex : LeftRightLine)
+				{
+					if (Vertex.y == Y)
+					{
+						Start = Vertex;
+						break;
+					}
+				}
+				for (auto Vertex : TopLeftLine)
+				{
+					if (Vertex.y == Y)
+					{
+						End = Vertex;
+						break;
+					}
+				}
+				DrawLine(Start, End, InImage, InColor);
+			}
+		}
+		*/
+
+		// the first attempt can work, but it complicates things.
+		// it depends 3 contour line segments drawing to compute vertex on line, then draw horizontal lines.
+		// another issue is that: we can apply line slope to calculate next pixel x, since we are increasing y by one pixel.
+		// but here fill the triangle DOES NOT need to consider the gaps occurs in x-axis. because we are increase y by one pixel,
+		// even it has big gaps for contour's x, we are draw horizontal lines to fill the region anyway, the gap will not appear.
+		// so here is second attempt.
+		
+		// sort 3 vertices by y descent.
+		// define 0 is top vertex.
+		if (InVert0.y < InVert1.y)
+		{
+			std::swap(InVert0, InVert1);
+		}
+		if (InVert0.y < InVert2.y)
+		{
+			std::swap(InVert0, InVert2);
+		}
+		if (InVert1.y < InVert2.y)
+		{
+			std::swap(InVert1, InVert2);
+		}
+
+		int firstSegments = InVert0.y - InVert1.y;
+		int index = 1;
+		float Slope01 = float(InVert1.x - InVert0.x) / float(InVert1.y - InVert0.y);
+		float Slope02 = float(InVert2.x - InVert0.x) / float(InVert2.y - InVert0.y);
+		float Slope12 = float(InVert2.x - InVert1.x) / float(InVert2.y - InVert1.y);
+
+		// Vert0-Vert1 segment
+		for (int Y = InVert0.y - 1; Y >= InVert1.y; Y--)
+		{
+			// be careful with float to int rounding.
+			int StartX = InVert0.x - Slope01*index;
+			int EndX = InVert0.x - Slope02*index;
+			DrawLine(StartX, Y, EndX, Y, InImage, InColor);
+			index++;
+		}
+		// Vert1-Vert2 segment
+		index = 1;
+		for (int Y = InVert1.y - 1; Y > InVert2.y; Y--)
+		{
+			// be careful with float to int rounding.
+			int StartX = InVert1.x - Slope12*index;
+			int EndX = InVert0.x - Slope02*(firstSegments + index);
+			DrawLine(StartX, Y, EndX, Y, InImage, InColor);
+			index++;
 		}
 	}
 };
--- Rasterizer/Source/Main.cpp
+++ Rasterizer/Source/Main.cpp
@@ -4,11 +4,13 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 
 namespace
 {
 	int Width = 800;
 	int Height = 800;
+
 	void DrawPixelTest(TGAImage& InImage)
 	{
 		//draw pixel
@@ -34,21 +36,42 @@ namespace
 			// draw 3 lines of each face.
 			for (int index = 0; index < 3; index++)
 			{
-				Vec3f FaceVertex1 = ModelData.vert(FaceData[index]);
-				Vec3f FaceVertex2 = ModelData.vert(FaceData[(index+1)%3]);
-				Line::DrawLine((FaceVertex1.x+1.)*InWidth/2., (FaceVertex1.y + 1.)*InHeight / 2., 
-					(FaceVertex2.x + 1.)*InWidth / 2., (FaceVertex2.y + 1.)*InHeight / 2., InImage, white);
+				Vec3f FaceVertex0 = ModelData.vert(FaceData[index]);
+				Vec3f FaceVertex1 = ModelData.vert(FaceData[(index+1)%3]);
+
+				int X0 = (FaceVertex0.x + 1.)*InWidth / 2.;
+				int Y0 = (FaceVertex0.y + 1.)*InHeight / 2.;
+				int X1 = (FaceVertex1.x + 1.)*InWidth / 2.;
+				int Y1 = (FaceVertex1.y + 1.)*InHeight / 2.;
+
+				Line::DrawLine(X0, Y0, X1, Y1, InImage, white);
 			}
 		}
+	}
+
+	void DrawTriangleTest(TGAImage& InImage)
+	{
+		Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+		Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+		Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+
+		//Line::DrawTriangle2D(t0[0], t0[1], t0[2], InImage, red);
+		//Line::DrawTriangle2D(t1[0], t1[1], t1[2], InImage, white);
+		//Line::DrawTriangle2D(t2[0], t2[1], t2[2], InImage, green);
+
+		Line::DrawAndFillTriangle2D(t0[0], t0[1], t0[2], InImage, red);
+		Line::DrawAndFillTriangle2D(t1[0], t1[1], t1[2], InImage, white);
+		Line::DrawAndFillTriangle2D(t2[0], t2[1], t2[2], InImage, green);
 	}
 }
 
 int main(int argc, char** argv) 
 {
-	TGAImage image(Width, Height, TGAImage::RGB);
+	TGAImage image(200, 200, TGAImage::RGB);
 
 	//DrawLineTest(image);
-	DrawModelWireFrameTest(Width, Height, image);
+	//DrawModelWireFrameTest(Width, Height, image);
+	DrawTriangleTest(image);
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
